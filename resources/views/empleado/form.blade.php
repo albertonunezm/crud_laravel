b0191fc9545f1ec0def8cf9ede28eba598cd757d
Alberto <albertonunezmaneiro@gmail.com>
2022-03-28 18:40:44 -0300

    Ajustes al formulario para insertar/actualizar

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,20 +1,20 @@
     <label for="nombre">Nombre</label>
-    <input type="text" value="{{ $empleado->nombre }}" name="nombre" id="nombre"> 
+    <input type="text" value="{{ isset($empleado->nombre)?$empleado->nombre:'' }}" name="nombre" id="nombre"> 
     
     <br>
 
     <label for="apellido">Apellido</label>
-    <input type="text" value="{{ $empleado->apellido }}" name="apellido" id="apellido"> 
+    <input type="text" value="{{ isset($empleado->apellido)?$empleado->apellido:'' }}" name="apellido" id="apellido"> 
     
     <br>
     
     <label for="rut">RUT</label>
-    <input type="text" value="{{ $empleado->rut }}" name="rut" id="rut"> 
+    <input type="text" value="{{ isset($empleado->rut)?$empleado->rut:'' }}" name="rut" id="rut"> 
     
     <br>
     
     <label for="correo">Correo</label>
-    <input type="email" value="{{ $empleado->correo }}" name="correo" id="correo"> 
+    <input type="email" value="{{ isset($empleado->correo)?$empleado->correo:'' }}" name="correo" id="correo"> 
     
     <br>
 
