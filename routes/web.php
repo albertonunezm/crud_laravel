b224198a82b58cc69e8e2300a3074d56283e1ebd
Alberto <albertonunezmaneiro@gmail.com>
2022-03-28 21:37:23 -0300

    Integrando Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/', function () {
 });*/
 
 Route::resource('empleado', EmpleadoController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
